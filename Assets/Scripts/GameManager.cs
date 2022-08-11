e6ce711360ee472c4eb9038a62046aa46bdb5b6c
hmuaz <muazsenol1@gmail.com>
2022-08-11 12:46:30 +0300

    DontDestroyOnLoad

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    static GameManager gm;
+
+
+    private void Start()
+    {
+        if(gm != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            gm = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+
+        }
+    }
     public void Nextlevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
