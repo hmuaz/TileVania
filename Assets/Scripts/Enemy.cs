b636f48bb033a7ffb74eaf4ac9aca5001747b9f4
hmuaz <muazsenol1@gmail.com>
2022-08-18 11:28:50 +0300

    Level2

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -17,7 +17,12 @@ public class Enemy : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "EnemyYon")
+        if (collision.gameObject.tag == "Player")
+        {
+            gm.GameOver();
+        }
+
+        if (collision.gameObject.tag == "EnemyYon")
         {
             if(enemyMovement == 1)
             {
@@ -37,13 +42,7 @@ public class Enemy : MonoBehaviour
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            gm.GameOver();
-        }
-    }
+    
 
     private void Update()
     {
